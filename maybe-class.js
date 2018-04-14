d0628936971272ad09e3705e567828f8f4d21f55
Kanit Sharma <kanit.sharma@praktice.ai>
2018-04-15 00:10:44 +0530

    Improvements

--- maybe-class.js
+++ maybe-class.js
@@ -1,8 +1,12 @@
-module.exports = Maybe = x => ({
-  of: x => Maybe(x),
-  isNothing: () => x === null || x === undefined,
-  map: f => x === null || x === undefined ? Maybe(null) : Maybe(f(x)),
-  chain: f => x === null || x === undefined ? Maybe(null) : Maybe(f(x)).fold(),
-  fold: () => x,
-  inspect: () => `Maybe(${x})`
-})
+const Maybe = x => {
+  const of = x => Maybe(x)
+  const isNothing = () => x === null || x === undefined
+  const map = f => isNothing() ? Maybe(null) : Maybe(f(x))
+  const chain = f => x === null || x === undefined ? Maybe(null) : Maybe(f(x)).fold()
+  const fold = () => x
+  const inspect = () => `Maybe(${x})`
+
+  return { of, isNothing, map, chain, fold, inspect }
+}
+
+module.exports = Maybe 
--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 const Maybe = require('./maybe-class')
 
-const res = Maybe(null)
+const res = Maybe(10)
   .map(x => x*10)
+  .map(x => Maybe(x*100))
+  .inspect()
 
 console.log(res)
