8b712180378a73d66af3ddc90378477df987bce4
kanitsharma <kanitshrm09@gmail.com>
2018-04-18 19:16:30 +0530

    State monad implementation from haskell

--- state.js
+++ state.js
@@ -1,7 +1,40 @@
-const State = s => {
-  const store = {}
-
-  const map = a => State(a(s))
-  const fold = _ => s
-
+function tate(s) {
+  this.runState = s;
 }
+
+State.prototype = {
+  map: function(f) {
+    var runState = this.runState;
+    return new State(function(state) {
+      var prev = runState(state);
+      return { value: f(prev.value), state: prev.state };
+    });
+  },
+
+  join: function() {
+    var runState = this.runState;
+    return new State(function(state) {
+      var prev = runState(state);
+      var inner = prev.value.runState(prev.state);
+      return inner;
+    });
+  },
+
+  bind: function(f) {
+    return this.map(f).join();
+  },
+
+  evalState: function(initState) {
+    var result = this.runState(initState);
+    return result.value;
+  },
+
+  execState: function(initState) {
+    var result = this.runState(initState);
+    return result.state;
+  }
+};
+
+const State = s => {
+  const map = f => Stte()) 
+}<Up>
